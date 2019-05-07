0aea6f0d57b87563d6f4f10192d881c67ea68e42
try-to <tryto@huahuashe.com>
2019-05-07 13:24:33 +0800

    update

--- src/Protocols/Rtmp.php
+++ src/Protocols/Rtmp.php
@@ -3,6 +3,7 @@
 namespace TrytoMediaServer\Protocols;
 
 use TrytoMediaServer\Protocols\ProtocolInterface;
+use TrytoMediaServer\Protocols\Rtmp\RtmpOperation;
 use TrytoMediaServer\Protocols\Rtmp\RtmpPacket;
 use TrytoMediaServer\Protocols\Rtmp\RtmpStream;
 
@@ -21,8 +22,8 @@ class Rtmp implements ProtocolInterface
     private static $prevReadingPacket = array();
 
     private static $chunkSizeR = 128, $chunkSizeW = 128;
-	
-	private static $operations = array();
+
+    private static $operations = array();
 
     /**
      * Check the integrity of the package.
@@ -57,12 +58,12 @@ class Rtmp implements ProtocolInterface
 
         $packet = self::readPacket($buffer);
         var_dump($packet);
-        
+
         return $buffer;
     }
 
     private static function readPacket($buffer)
-	{
+    {
         $packet = new RtmpPacket();
         $header = self::readBuffer($buffer, 0)->readTinyInt();
 
@@ -85,11 +86,14 @@ class Rtmp implements ProtocolInterface
         switch ($packet->chunkType) {
             case RtmpPacket::CHUNK_TYPE_3:
                 $packet->timestamp = self::$prevReadingPacket[$packet->chunkStreamId]->timestamp;
+            // no break
             case RtmpPacket::CHUNK_TYPE_2:
                 $packet->length = self::$prevReadingPacket[$packet->chunkStreamId]->length;
                 $packet->type = self::$prevReadingPacket[$packet->chunkStreamId]->type;
+            // no break
             case RtmpPacket::CHUNK_TYPE_1:
                 $packet->streamId = self::$prevReadingPacket[$packet->chunkStreamId]->streamId;
+            // no break
             case RtmpPacket::CHUNK_TYPE_0:
                 break;
         }
@@ -101,31 +105,51 @@ class Rtmp implements ProtocolInterface
             $packet->hasAbsTimestamp = true;
         }
 
+        if (!isset(self::$operations[$packet->chunkStreamId])) {
+            self::$operations[$packet->chunkStreamId] = new RtmpOperation();
+        }
+
+        if (self::$operations[$packet->chunkStreamId]->getResponse()) {
+            //Operation chunking....
+            $packet = self::$operations[$packet->chunkStreamId]->getResponse()->getPacket();
+            $headerSize = 0; //no header
+        } else {
+            //Create response from packet
+            self::$operations[$packet->chunkStreamId]->createResponse($packet);
+        }
+
         $headerSize--;
-		$header;
-        
-        if($headerSize>0)
-			$header = self::readBuffer($buffer, $headerSize);
-		if($headerSize >= 3)
-			$packet->timestamp = $header->readInt24();
-		if($headerSize >= 6)
-		{
-			$packet->length = $header->readInt24();
-			
-			$packet->bytesRead = 0;
-			$packet->free();
-		}
-		if($headerSize > 6)
-			$packet->type = $header->readTinyInt();
-			
-		if($headerSize == 11)
-			$packet->streamId = $header->readInt32LE();
-		
-		$nToRead = $packet->length - $packet->bytesRead;
-		$nChunk = self::$chunkSizeR;
-		if($nToRead < $nChunk)
-			$nChunk = $nToRead;
-        if($packet->isReady()){
+        $header;
+
+        if ($headerSize > 0) {
+            $header = self::readBuffer($buffer, $headerSize);
+        }
+
+        if ($headerSize >= 3) {
+            $packet->timestamp = $header->readInt24();
+        }
+
+        if ($headerSize >= 6) {
+            $packet->length = $header->readInt24();
+
+            $packet->bytesRead = 0;
+            $packet->free();
+        }
+        if ($headerSize > 6) {
+            $packet->type = $header->readTinyInt();
+        }
+
+        if ($headerSize == 11) {
+            $packet->streamId = $header->readInt32LE();
+        }
+
+        $nToRead = $packet->length - $packet->bytesRead;
+        $nChunk = self::$chunkSizeR;
+        if ($nToRead < $nChunk) {
+            $nChunk = $nToRead;
+        }
+
+        if ($packet->isReady()) {
             return $packet;
         }
 
